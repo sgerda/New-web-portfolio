7031ec4289658c59deba1b3d6d241db05e14aa02
sgerda <135041922+sgerda@users.noreply.github.com>
2026-01-25 16:23:58 -0500

    Projects cards and initial render draft complete( need styling)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { provideRouter, RouterOutlet } from '@angular/router';
 import { SideBar } from "./side-bar/side-bar";
 import { TopBar } from './top-bar/top-bar';
+import { Routes } from '@angular/router';
+import bootstrap from '../main.server';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { ProjectsTab, ProjectsTabComponent } from './projects-tab/projects-tab';
+import { routes } from './app.routes';
+
+
+// bootstrapApplication(Component, { providers: [provideRouter(
+//     routes
+// )] })
+
 
 @Component({
     selector: 'app-root',
+    standalone: true,
     imports: [RouterOutlet, SideBar, TopBar], 
     templateUrl: './app.html',
     styleUrl: './app.css'
 })
+
+
 export class App { }
--- src/app/projects-tab/projects-tab.ts
+++ src/app/projects-tab/projects-tab.ts
@@ -1,11 +1,95 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Directive, ElementRef, Renderer2, 
+  Inject, PLATFORM_ID, OnInit, OnDestroy, Input, 
+  forwardRef } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+
+
+
+interface CardData {
+  id: number;
+  title: string; 
+  description: string;
+  image: string;
+  color?: string;
+}
+
+
+@Directive({
+  selector: '[appProjectsTab]',
+  standalone: true
+})
+export class ProjectsTab implements OnInit, OnDestroy{
+  @Input() stackFactor = 0.05;
+  @Input() blurMax = 3;
+
+  private observer!: IntersectionObserver;
+
+  constructor(private el: ElementRef, private renderer: Renderer2,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
+
+  ngOnInit() {
+    
+    if (isPlatformBrowser(this.platformId)) {
+      const options = {
+        root: null,
+        threshold: this.createThresholds(100)
+      };
+
+      this.observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+          const ratio = entry.intersectionRatio;
+          const scale = 1 - ((1 - ratio) * this.stackFactor);
+          const blur = (1 - ratio) * this.blurMax;
+          const opacity = 0.5 + (ratio * 0.5);
+
+          this.renderer.setStyle(this.el.nativeElement, 'transform', `scale(${scale})`);
+          this.renderer.setStyle(this.el.nativeElement, 'filter', `blur(${blur}px)`);
+          this.renderer.setStyle(this.el.nativeElement, 'opacity', opacity);
+        });
+      }, options);
+
+      this.observer.observe(this.el.nativeElement);
+    }
+  }
+
+  private createThresholds(num: number){
+    return Array.from({length: num +1}, (_,i) => i/num).reverse();
+  }
+  ngOnDestroy(){
+    if (isPlatformBrowser(this.platformId) && this.observer) {
+      this.observer.disconnect();
+    }
+
+    
+  }
+
+}
 
 @Component({
   selector: 'app-projects-tab',
-  imports: [],
+  standalone: true,
+  imports: [ProjectsTab, CommonModule],
   templateUrl: './projects-tab.html',
   styleUrl: './projects-tab.css',
 })
-export class ProjectsTab {
 
+export class ProjectsTabComponent {
+  cards: CardData[] = [
+    {
+      id: 1,
+      title: 'Project 1',
+      description: 'First project description',
+      image: 'assets/images/p1.jpg',
+      color: '#ce3535'
+    },
+    {
+      id: 2,
+      title: 'Project 2',
+      description: 'Second project description',
+      image: 'assets/images/p2.jpg',
+      color: '#7e3db3'
+    }
+  ];
 }
