77570248d706e25bd22d8490e272cb59302c0f6c
sgerda <135041922+sgerda@users.noreply.github.com>
2026-02-03 18:30:06 -0500

    refactorinf need to be complete

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -24,11 +24,12 @@ interface TimelineEntry {
 })
 export class About {
 
+  // NOTE: Complete refactoring the timeline component to support sub-headers
   infoEntries: TimelineEntry[] = [
     {
       year: '2010',
       description: 'In 2010, My family decided to moved to the United States from Dominican Republic. This was a big change for me as I had to adapt to a new culture and language.',
-      bulletPoints: 'none'
+      bulletPoints: ''
     }
   ];
 
